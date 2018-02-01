3fd17894f3d1b4163c0cb6de4aa529c020402b29
Junior Dotson <dotnet.learner.junior@gmail.com>
2018-02-01 11:45:08 -0500

    Initial checkin.

---
+++ SpyStore.DAL/EF/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using SpyStore.Models.Entities;
+
+namespace SpyStore.DAL.EF
+{
+    public class StoreContext :DbContext
+    {
+        public StoreContext()
+        {
+        }
+
+        public StoreContext( DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
